a1be926086c037741fcd18ba79b4d3c3d87045b7
Jhon Churivanti Alva <114524940+Jhonchuri11@users.noreply.github.com>
2024-05-10 16:44:09 -0500

    Add files via upload

---
+++ Flask/app/app.py
@@ -0,0 +1,13 @@
+# Importamos flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Creando una ruta y renderizando una visa
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True, port=5000)
